c2a1344e2493fd8c3cadba78e38783f5e0d2a77d
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-03-24 20:41:20 +0100

    Completed creating design for OneOrder component.

--- src/components/OneOrder.tsx
+++ src/components/OneOrder.tsx
@@ -1,37 +1,80 @@
 import { type IOrder } from "../store/ProductsContext";
 
 export default function OneOrder({ order }: { order: IOrder }) {
+  const orderColor =
+    order.status === "Delivered"
+      ? " bg-green-400/60"
+      : order.status === "In Transit"
+        ? "bg-blue-400/60"
+        : order.status === "Processing"
+          ? "bg-yellow-400/60"
+          : order.status === "Processing"
+            ? "bg-orange-400/60"
+            : "";
+
+  const summProd = order.items.reduce(
+    (acc, product) => acc + product.quantity,
+    0,
+  );
+
+  const twoProd =
+    order.items.length >= 2 ? order.items.slice(0, 2) : order.items;
+
+  const summRestProd =
+    summProd - twoProd.reduce((acc, product) => acc + product.quantity, 0);
+
   return (
-    <div className="border border-zinc-400/70 bg-amber-100/20 rounded-md py-2 px-3">
-      <div className="flex justify-between">
-        <p>{order.id}</p>
-        <p>{order.status}</p>
+    <div className="flex flex-col border border-zinc-400/70 bg-amber-100/20 rounded-md py-2 px-3 text-base h-full">
+      <div className="flex justify-between items-center mb-2">
+        <p className="line-clamp-1">{order.id}</p>
+        <p
+          className={`${orderColor} rounded-full px-2 tracking-tight line-clamp-1`}
+        >
+          {order.status}
+        </p>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center mb-3">
         <p>{order.date}</p>
         <div className="flex flex-col">
           <p>Total Price:</p>
           <p>{order.totalPrice}</p>
         </div>
       </div>
-      <ul className="flex  gap-2">
-        {order.items.map((item) => (
-          <li key={item.id} className="w-30 aspect-4/5 overflow-hidden py-2">
-            <img className="w-16 h-16 object-cover rounded-md border border-zinc-200" src={item.image} alt={item.name} />
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {order.items.map((item) => (
-          <li key={item.id}>
-            <p>{item.name}</p>
+      <div className="h-20 mb-2">
+        <ul className="flex justify-center gap-4">
+          {twoProd.map((item) => (
+            <li
+              key={item.id}
+              className="flex justify-center overflow-hidden py-2"
+            >
+              <img
+                className="w-16 h-16 object-cover rounded-md border border-zinc-200"
+                src={item.image}
+                alt={item.name}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <ul className="">
+        {twoProd.map((item) => (
+          <li key={item.id} className="flex justify-between items-center my-2">
+            <p className="line-clamp-1">{item.name}</p>
             <p>{item.quantity}</p>
           </li>
         ))}
+        <p className="flex justify-end my-2">
+          {summRestProd > 0 && `+${summRestProd}`}
+        </p>
       </ul>
-      <div>
-        <button>view details</button>
-        <button>reorder</button>
+      <div className="flex justify-between items-center py-1 mt-auto">
+        <button className="flex my-3 w-[60%] mb-2 p-1 uppercase border border-zinc-300 shadow-[0_0_10px_2px_rgba(255,180,0,0.5)] bg-yellow-500/20 rounded-xl cursor-pointer whitespace-nowrap hover:bg-yellow-500/30  transition-colors duration-300 active:scale-95 ">
+          view details
+        </button>
+        <button className="flex underline text-sm text-zinc-900 cursor-pointer hover:text-black  transition-colors duration-300 active:scale-95">
+          reorder
+        </button>
       </div>
     </div>
   );
--- src/pages/account pages/OrderHistory.tsx
+++ src/pages/account pages/OrderHistory.tsx
@@ -6,7 +6,7 @@ export default function OrderHistory() {
   return (
     <div className="border border-zinc-400/30 bg-amber-100/20 rounded-md py-2 px-3 cursor-default">
       <h2 className="text-xl m-2">Your Orders</h2>
-      <ul className="grid grid-cols-2 gap-3">
+      <ul className="grid grid-cols-2 gap-3 items-stretch auto-rows-fr">
         {userOrders.map((order) => (
           <li key={order.id}>
             <OneOrder order={order} />
